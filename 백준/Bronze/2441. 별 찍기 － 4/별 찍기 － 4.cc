1723f1e35984a06c3e1030393062a888386b06ae
Siyun Lee <siyunkr@gmail.com>
2025-01-03 21:13:53 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	for(int i=1; i<=n; i++){
+		for(int j=0; j<i-1; j++){
+			cout << " ";
+		}
+		for(int j=n; j>=i; j--){
+			cout << "*";
+		}
+		cout << '\n';
+	}
+}
