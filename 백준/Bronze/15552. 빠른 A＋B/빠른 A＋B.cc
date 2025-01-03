bb674b916542153ac7be0b9ed29fcd7bb98c3288
Siyun Lee <siyunkr@gmail.com>
2025-01-03 20:47:44 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int t;
+	cin >> t;
+	
+	for(int i=0; i<t; i++){
+		int a, b;
+		cin >> a >> b;
+		
+		cout << a + b << '\n';
+	}
+}
