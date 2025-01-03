e8e0601d35e2f063e9ae587197ea7a17019340d1
Siyun Lee <siyunkr@gmail.com>
2025-01-03 20:19:22 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+	
+	vector<int> times;
+	
+	for(int i=0; i<n; i++){
+		int time;
+		cin >> time;
+		times.push_back(time);
+	}
+	
+	int ysum = 0;
+	int msum = 0;
+	
+	for(int x : times) {
+		int y = 10 + (x/30) * 10;
+		int m = 15 + (x/60) * 15;
+		
+		ysum += y;
+		msum += m;
+	}
+	
+	if(ysum < msum) {
+		cout << "Y" << " " << ysum;
+	}else if(msum < ysum) {
+		cout << "M" << " " << msum;
+	}else if(ysum == msum) {
+		cout << "Y M" << " " << ysum;
+	}	
+}
