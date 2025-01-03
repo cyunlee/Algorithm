dcabf4bf75c7a1c6c397f3a9361e338db36aedf4
Siyun Lee <siyunkr@gmail.com>
2025-01-03 16:13:18 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s;
+int ret[26];
+
+int main() {
+	cin >> s;
+	
+	for(char x : s) {
+		int idx = x - 97;
+		ret[idx]++;
+	}
+	
+	for(int num : ret) {
+		cout << num << ' ';
+	}
+	
+	return 0;
+}
