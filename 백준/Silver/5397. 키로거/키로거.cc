753e95575e384ee31bf36a0037277cf11dbbcc93
Siyun Lee <siyunkr@gmail.com>
2025-01-06 21:20:05 +0900

    [Silver II] Title: 키로거, Time: 252 ms, Memory: 19384 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int T;
+
+bool isUsable(char c){
+	if(isdigit(c) || isalpha(c)) return true;
+	return false;
+}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> T;
+	string bufferflush;
+	getline(cin, bufferflush);
+	
+	for(int i=0; i<T; i++){
+		string pwd;
+		string ans;
+		getline(cin, pwd);
+		
+		list<char> L;
+		auto cursor = L.begin();
+		
+		for(char x : pwd){
+			if(x=='<' && cursor!=L.begin()) {
+				cursor--;
+			}
+			else if(isUsable(x)){
+				L.insert(cursor, x);
+			}
+			else if(x=='>' && cursor!=L.end()){
+				cursor++;
+			}
+			else if(x=='-' && cursor!=L.begin()){
+				cursor = L.erase(prev(cursor));
+			}
+		}
+		
+		for(auto i : L) cout << i;
+		cout << '\n';	
+	}		
+}
