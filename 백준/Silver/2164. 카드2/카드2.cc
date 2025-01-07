7476f4f3a308d0753be5154096885c797bd24a65
Siyun Lee <siyunkr@gmail.com>
2025-01-07 14:09:18 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int N;
+	cin >> N;
+	
+	queue<int> Q;
+	
+	for(int i=1; i<=N; i++) {
+		Q.push(i);
+	}
+	
+	while(Q.size()>1){
+		Q.pop();
+		int tmp = Q.front();
+		Q.pop();
+		Q.push(tmp);
+	}
+	
+	cout << Q.front();
+}
