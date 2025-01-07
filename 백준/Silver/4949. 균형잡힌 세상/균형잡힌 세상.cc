f7f6812bc7c6f1493890dc6bf9d398aaf151005a
Siyun Lee <siyunkr@gmail.com>
2025-01-07 17:31:22 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+string str;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	while(true){
+		getline(cin, str, '.');
+		string bufferflush;
+		getline(cin, bufferflush);
+		
+		bool isBalanced = false;
+		
+		if(str=="") break;
+		
+		stack<char> S;
+		
+		for(char x : str) {
+			if(x == '('||x == '[') S.push(x);
+			else if(x == ')'){
+				if(!S.empty() && S.top() == '(') {
+					S.pop();
+				}else S.push(x);
+			}
+			else if(x == ']'){
+				if(!S.empty() && S.top() == '[') {
+					S.pop();
+				}else S.push(x);
+			}
+		}
+		
+		if(S.empty()) isBalanced = true;
+		else isBalanced = false;
+		
+		if(isBalanced){
+			cout << "yes" << '\n';
+		}else {
+			cout << "no" << '\n';
+		}
+	}
+}
