843ff9804ef1ede746fef04ce3decf3d77732647
Siyun Lee <siyunkr@gmail.com>
2025-01-08 19:17:37 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -4,37 +4,29 @@ using namespace std;
 #define Y second
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-int dist[102][102];
-int board[102][102];
 int n, m;
+char board[102][102];
+int dist[102][102];
 
 int main(void){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
 	cin >> n >> m;
 	
-	//미로 입력받기
+	//미로 입력
 	for(int i=0; i<n; i++){
-		string row;
-		cin >> row;
-		
+		string str;
+		cin >> str;
 		for(int j=0; j<m; j++){
-			board[i][j] = row[j];
-		}
-	}
-	
-	//dist 배열을 -1로 초기화
-	for(int i=0; i<n; i++){
-		for(int j=0; j<m; j++){
-			dist[i][j] = -1; 
+			board[i][j] = str[j];
+			if(board[i][j]=='1') dist[i][j] = -1;
 		}
 	}
 	
 	queue<pair<int, int>> Q;
-
-	dist[0][0] = 1; //시작점
+	//(1,1) ~ (n, m) -> board[0][0] ~ board[n-1][m-1]
 	Q.push({0, 0});
+	dist[0][0] = 0;
 	
 	while(!Q.empty()){
 		pair<int, int> cur = Q.front();
@@ -44,13 +36,12 @@ int main(void){
 			int nx = cur.X + dx[dir];
 			int ny = cur.Y + dy[dir];
 			
-			if(nx < 0 || nx >= n || ny < 0 || ny>=m) continue;
-			if(dist[nx][ny] != -1 || board[nx][ny]=='0') continue;
+			if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+			if(dist[nx][ny]>=0 || board[nx][ny]=='0') continue;
 			
 			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
 			Q.push({nx, ny});
 		}
 	}
-	
-	cout << dist[n-1][m-1];
+	cout << dist[n-1][m-1] + 1;
 }
